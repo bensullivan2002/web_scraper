754adc94f782e5909fa7c73144f913b89721401d
bensullivan2002 <bensullivan2002@gmail.com>
2017-11-02 14:40:06 +0000

    Changed initial input prompt

--- web_scraper1.py
+++ web_scraper1.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 from selenium.webdriver.common.keys import Keys
 
-search_string = input("Search for a specialist in which field?").lower()
+search_string = input("Search for a specialist in which field? ['cardiology' or 'diagnostic imaging'] ").lower()
 
 if search_string == "cardiology":
     url = "https://findavet.rcvs.org.uk/find-a-vet-surgeon/?filter-keyword=&filter-searchtype=surgeon&specialist5=true"
