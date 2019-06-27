f25874c9f522de372dfca198937adb1410902f0b
Ben Sullivan <ben.sullivan@imv-imaging.com>
2019-06-27 17:50:48 +0100

    branch p= trial

--- web_scraper1/beautifulsoup_scraper1.py
+++ web_scraper1/beautifulsoup_scraper1.py
@@ -16,42 +16,75 @@ specialist_areas = []
 start_time = time()
 request_count = 1
 
-pages = [str(i) for i in range(1,6)]
+for i in range(1,6):
 
-for i in (pages):
-
-    url = 'https://findavet.rcvs.org.uk/find-a-vet-surgeon/?filter-choice=&filter-keyword=&filter-searchtype=surgeon&specialist5=true&%p=' + i
-
-    response = get(url)
-
-    sleep(randint(1, 3))
-
-    request_count += 1
-    elapsed_time = time() - start_time
-    print('Request: {}; Frequency: {} Requests/s;'.format(request_count, request_count / elapsed_time))
-    clear_output(wait=True)    
+    if i == 1:
     
-    html_soup = BeautifulSoup(response.text, 'lxml')
+        url = 'https://findavet.rcvs.org.uk/find-a-vet-surgeon/?filter-choice=&filter-keyword=&filter-searchtype=surgeon&specialist5=true&%p='
 
-    result_containers = html_soup.find_all('div', class_='item item--fav item--surgeon')
-    specialism = html_soup.find_all('div', class_='item-additional')
+        response = get(url)
 
-    for container in result_containers:
-        name = container.div.h3.a.text
-        names.append(name)
+        sleep(randint(1, 3))
 
-        qualification = container.div.span.text
-        qualifications.append(qualification)
+        elapsed_time = time() - start_time
+        print('Request: {}; Frequency: {} Requests/s;'.format(request_count, request_count / elapsed_time))
+        clear_output(wait=True)    
+        
+        html_soup = BeautifulSoup(response.text, 'lxml')
 
-    for container in specialism:
-        speciality = container.li.text
-        specialist_areas.append(speciality)    
-    
-    from pandas import ExcelWriter
-    test_df = pd.DataFrame({'Name': names,
-    'Qualifications': qualifications,
-    'Specialist in:': specialist_areas})
-    print(test_df.info())
+        result_containers = html_soup.find_all('div', class_='item item--fav item--surgeon')
+        specialism = html_soup.find_all('div', class_='item-additional')
+
+        for container in result_containers:
+            name = container.div.h3.a.text
+            names.append(name)
+
+            qualification = container.div.span.text
+            qualifications.append(qualification)
+
+        for container in specialism:
+            speciality = container.li.text
+            specialist_areas.append(speciality)    
+        
+        from pandas import ExcelWriter
+        test_df = pd.DataFrame({'Name': names,
+        'Qualifications': qualifications,
+        'Specialist in:': specialist_areas})
+        print(test_df.info())
+
+    else:
+
+        url = 'https://findavet.rcvs.org.uk/find-a-vet-surgeon/?filter-choice=&filter-keyword=&filter-searchtype=surgeon&specialist5=true&%p=' + str(i)
+
+        response = get(url)
+
+        sleep(randint(1, 3))
+
+        elapsed_time = time() - start_time
+        print('Request: {}; Frequency: {} Requests/s;'.format(request_count, request_count / elapsed_time))
+        clear_output(wait=True)    
+        
+        html_soup = BeautifulSoup(response.text, 'lxml')
+
+        result_containers = html_soup.find_all('div', class_='item item--fav item--surgeon')
+        specialism = html_soup.find_all('div', class_='item-additional')
+
+        for container in result_containers:
+            name = container.div.h3.a.text
+            names.append(name)
+
+            qualification = container.div.span.text
+            qualifications.append(qualification)
+
+        for container in specialism:
+            speciality = container.li.text
+            specialist_areas.append(speciality)    
+        
+        from pandas import ExcelWriter
+        test_df = pd.DataFrame({'Name': names,
+        'Qualifications': qualifications,
+        'Specialist in:': specialist_areas})
+        print(test_df.info())
     
 writer = pd.ExcelWriter('beautiful_soup_output.xlsx')
 test_df.to_excel(writer, 'Cardiologists')
